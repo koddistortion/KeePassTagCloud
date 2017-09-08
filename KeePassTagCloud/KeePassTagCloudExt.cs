62e4f950e68f1e82dcd80a2d0222da6a706f0cec
Oliver Kotte <dev@koddistortion.net>
2017-09-08 13:48:09 +0200

    initial commit

---
+++ KeePassTagCloud/KeePassTagCloudExt.cs
@@ -0,0 +1,16 @@
+﻿using KeePass.Plugins;
+
+namespace KeePassTagCloud
+{
+    public sealed class KeePassTagCloudExt : Plugin
+    {
+        private IPluginHost pluginHost;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            this.pluginHost = host;
+
+            return true;
+        }
+    }
+}
